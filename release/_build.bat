1b76a9a34a7d2bb916c35e0e85d98c828cabc84a
Pavel <pavel.beran@centrum.cz>
2020-11-06 16:26:09 +0100

    berybox

--- release/_build.bat
+++ release/_build.bat
@@ -4,19 +4,30 @@ set KECVERSION=v1
 set GOARCH=386
 
 set GOOS=linux
-go build -o ../release/kec-%KECVERSION%-linux-x86
+go build -o ../release/kec-%KECVERSION%-%GOOS%-x86
+c:\utils\7-Zip\7z.exe a -tzip -mmt8 -sdel ../release/kec-%KECVERSION%-%GOOS%-x86.zip ../release/kec-%KECVERSION%-%GOOS%-x86
+
 set GOOS=darwin
-go build -o ../release/kec-%KECVERSION%-darwin-x86
+go build -o ../release/kec-%KECVERSION%-%GOOS%-x86
+c:\utils\7-Zip\7z.exe a -tzip -mmt8 -sdel ../release/kec-%KECVERSION%-%GOOS%-x86.zip ../release/kec-%KECVERSION%-%GOOS%-x86
+
 set GOOS=windows
-go build -o ../release/kec-%KECVERSION%-windows-x86.exe
+go build -o ../release/kec-%KECVERSION%-%GOOS%-x86.exe
+c:\utils\7-Zip\7z.exe a -tzip -mmt8 -sdel ../release/kec-%KECVERSION%-%GOOS%-x86.zip ../release/kec-%KECVERSION%-%GOOS%-x86.exe
 
 set GOARCH=amd64
 
 set GOOS=linux
-go build -o ../release/kec-%KECVERSION%-linux-x64
+go build -o ../release/kec-%KECVERSION%-%GOOS%-x64
+c:\utils\7-Zip\7z.exe a -tzip -mmt8 -sdel ../release/kec-%KECVERSION%-%GOOS%-x64.zip ../release/kec-%KECVERSION%-%GOOS%-x64
+
 set GOOS=darwin
-go build -o ../release/kec-%KECVERSION%-darwin-x64
+go build -o ../release/kec-%KECVERSION%-%GOOS%-x64
+c:\utils\7-Zip\7z.exe a -tzip -mmt8 -sdel ../release/kec-%KECVERSION%-%GOOS%-x64.zip ../release/kec-%KECVERSION%-%GOOS%-x64
+
 set GOOS=windows
-go build -o ../release/kec-%KECVERSION%-windows-x64.exe
+go build -o ../release/kec-%KECVERSION%-%GOOS%-x64.exe
+c:\utils\7-Zip\7z.exe a -tzip -mmt8 -sdel ../release/kec-%KECVERSION%-%GOOS%-x64.zip ../release/kec-%KECVERSION%-%GOOS%-x64.exe
 
 cd ../release
+
